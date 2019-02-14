992c7a873ad85f93eeb258137400032e08fd93ac
Krushon <email5000@yandex.ru>
2019-02-14 12:36:04 +0300

    обновление списка пакетов

--- 2-script-install.sh
+++ 2-script-install.sh
@@ -4,10 +4,10 @@ SECONDS=0
 printf "\033c"
 echo "Устанавливаем необходимое ПО..."
 apt-get update
-apt-get install mc ntpdate ntp net-tools -y
+apt-get install mc net-tools ntp ntpdate ntpstat -y
 apt-get autoclean && apt-get clean
-#/etc/init.d/ntp stop && ntpdate pool.ntp.org && /etc/init.d/ntp start
-# Изменяем временную зону
+/etc/init.d/ntp stop
+# Изменяем временную зону на свою
 ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 echo
 echo -e "***** Script \033[33;1m2\033[0m of \033[33;1m3\033[0m COMPLETED in $SECONDS seconds *****"
